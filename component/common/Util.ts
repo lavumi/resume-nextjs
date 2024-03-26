f4fc84e882eb293fec1f534c3ab99a4c9f5419cb
Lavumi <35640315+lavumi@users.noreply.github.com>
2024-03-26 21:04:08 +0900

    update date format

--- component/common/Util.ts
+++ component/common/Util.ts
@@ -4,7 +4,7 @@ import { DateTime } from 'luxon';
 enum LUXON_DATE_FORMAT {
   YYYY_LL_DD = 'yyyy-LL-dd',
   YYYY_LL = 'yyyy-LL',
-  YYYY_DOT_LL = 'yyyy. LL',
+  YYYY_DOT_LL = 'yyyy.LL',
   YYYY_DOT_LL_DOT_DD = 'yyyy. LL. dd',
   KINDNESS_FULL = 'DDDD',
 
